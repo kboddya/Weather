daf13facb8f82846ee163be392c513009e93ad1a
BOKO <bohdanschool333@gmail.com>
2021-10-26 14:29:36 +0300

    v.4 Заміна на своє повідомлення

--- Weather.py
+++ Weather.py
@@ -1,23 +1,22 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('1328514129:AAFTw6SJ_ri42qHTWFHZN7A1u3GJEOKrwRs');
+bot = telebot.TeleBot('1328514129:AAFTw6SJ_ri42qHTWFHZN7A1u3GJEOKrwRs')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text =="Прогноз" or message.text == "/start":
 
-        markup = types.InlineKeyboardMarkup()
-        buttonA = types.InlineKeyboardButton('A', callback_data='a')
-        buttonB = types.InlineKeyboardButton('B', callback_data='b')
-        buttonC = types.InlineKeyboardButton('C', callback_data='c')
+      markup = types.ReplyKeyboardMarkup()
+      buttonA = types.KeyboardButton('Прогноз')
+      buttonB = types.KeyboardButton('/start')
+      buttonC = types.KeyboardButton('/help')
 
-        markup.row(buttonA, buttonB)
-        markup.row(buttonC)
+      markup.row(buttonA, buttonB)
+      markup.row(buttonC)
+      bot.send_message(message.chat.id, 'Впишы название города для получения прогноза', reply_markup=markup)
+      bot.send_message(message.from_user.id, "Впишы название города для получения прогноза")
 
-        bot.send_message(message.chat.id, 'It works!', reply_markup=markup)
-        bot.send_message(message.from_user.id, "Впишы название города для получения прогноза")
-    
     elif message.text =="/help":
         bot.send_message(message.from_user.id, "Напиши прогноз")
     else:
