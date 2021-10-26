1b324a1b56ef9b61e038166de73503d3b07338bc
BOKO <bohdanschool333@gmail.com>
2021-10-26 14:12:30 +0300

    v.2

--- Weather.py
+++ Weather.py
@@ -1,9 +1,19 @@
 import telebot;
+from telebot import types
 bot = telebot.TeleBot('1328514129:AAFTw6SJ_ri42qHTWFHZN7A1u3GJEOKrwRs');
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text =="Прогноз" or message.text == "/start":
 
+    markup = types.InlineKeyboardMarkup()
+    buttonA = types.InlineKeyboardButton('A', callback_data='a')
+    buttonB = types.InlineKeyboardButton('B', callback_data='b')
+    buttonC = types.InlineKeyboardButton('C', callback_data='c')
+
+    markup.row(buttonA, buttonB)
+    markup.row(buttonC)
+
+  bot.send_message(message.chat.id, 'It works!', reply_markup=markup)
         bot.send_message(message.from_user.id,"Впишы название города для получения прогноза")
     elif message.text =="/help":
         bot.send_message(message.from_user.id,"Напиши прогноз")
