caa0dc91151b8d62bf118cd3532032abe89620f4
BOKO <bohdanschool333@gmail.com>
2021-10-28 14:39:14 +0300

    Fix main.py: Add none stop bot polling

--- main.py
+++ main.py
@@ -78,6 +78,6 @@ def handle(call):
     bot.send_message(call.message.chat.id, "Вибери місто", reply_markup=get_markup())
     bot.answer_callback_query(call.id)
 
-#Hi
+#Hi, I am student
 
 bot.polling(none_stop=True, interval=0)
