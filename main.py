807bd16368f4ce6ae65e8ca85d7e9c64ccd6d0f4
BOKO <bohdanschool333@gmail.com>
2021-10-28 19:47:35 +0300

    Fix main.py: Add rounding of data

--- main.py
+++ main.py
@@ -78,6 +78,5 @@ def handle(call):
     bot.send_message(call.message.chat.id, "Вибери місто", reply_markup=get_markup())
     bot.answer_callback_query(call.id)
 
-#Hi, I am student
 
 bot.polling(none_stop=True, interval=0)
